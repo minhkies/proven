9b065185352bd6454bd697f8203d840cd875b1b9
abhimannyucc <abhimannyucc@gmail.com>
2020-02-19 11:42:06 -0800

    Added required and Asterix style

--- src/comps/InputField/index.js
+++ src/comps/InputField/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../font.scss"
-export default function InputField({title,placeholder,width}) {
+export default function InputField({title,placeholder,width,required,asterix}) {
     return(
         <div className={"input-field-container"}>
-            <p className={"input-title"}>{title}</p>
-            <input style={{width:width}} className={"input-field"} placeholder={placeholder}/>
+            <p className={"input-title"}>{title} {asterix?<span className={(asterix&&"input-field-asterix")}>*</span>:""}</p>
+            <input style={{width:width}} className={"input-field" + " " + (required&&"input-field-required")} placeholder={placeholder}/>
         </div>
         )
 }
@@ -12,5 +12,7 @@ export default function InputField({title,placeholder,width}) {
 InputField.defaultProps = {
     title:"",
     placeholder:"",
-    width:"100%"
+    width:"100%",
+    required:false,
+    asterix:false
 };
