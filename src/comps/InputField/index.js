414180ae2a4b8786b3759cd2b9febbe74b9ecdea
abhimannyucc <abhimannyucc@gmail.com>
2020-02-20 16:46:51 -0800

    Ficed some errors

--- src/comps/InputField/index.js
+++ src/comps/InputField/index.js
@@ -1,17 +1,24 @@
 import React,{useState} from "react";
 import "../font.scss";
 import * as FeatherIcon from "react-icons/fi";
+const allowedChars = "1234567890()- ";
 export default function InputField({title,placeholder,width,required,asterix,number,addbutton}) {
     let Icon=FeatherIcon["FiPlus"];
     const [phone, setPhone] = useState("");
    var formatedNumber = (event) =>{
-       if (number){
         let val = event.target.value;
-        val = val.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
-       setPhone(val);
+       if (number){
+        var char = val[val.length-1];
+        console.log(char);
+        if (val.length==10){
+            val = val.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+        }
+        if (allowedChars.indexOf(char)!==-1|| val.length==0){
+            setPhone(val);
+        }
        }
         else {
-            setPhone("");
+            setPhone(val);
         }
     }
     
@@ -20,10 +27,10 @@ export default function InputField({title,placeholder,width,required,asterix,num
             <p className={"input-title"}>{title} {asterix?<span className={(asterix&&"input-field-asterix")}>*</span>:""}</p>
             <div className = {"input-icon-cont"}>
             <input
-            maxLength={number?15:255}
+            maxLength={number?14:255}
             pattern={number&&"[0-9 ]+"}
             onChange={formatedNumber}
-            value={number?phone:undefined}
+            value={phone}
             type="text"
             style={{width}} 
             className={"input-field" + " " + (required&&"input-field-required")}
