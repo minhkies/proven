b23894e7bae829192104c5b7ab6a28004a7f7c01
Tin Hoang <minhkies@gmail.com>
2020-02-22 13:18:16 -0800

    Updating

--- src/comps/CategoryBtn/index.js
+++ src/comps/CategoryBtn/index.js
@@ -1,28 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import * as FeatherIcon from "react-icons/fi";
 import "../font.scss"
-export default function CategoryBtn({name,selected,menuIcon}) {
+export default function CategoryBtn({name, type, selected, menuIcon}) {
+    let [removed, setRemoved] = useState(false);
+    let [select, setSelect] = useState(false);
+
     let Icon = FeatherIcon[menuIcon];
     let btnStyle = null;
     let iconStyle = null;
 
-    if (selected==="activeBtn"){
-        btnStyle= "category-btn-container-select";
-        iconStyle="white-plus";
-        Icon=FeatherIcon["FiCheck"];
-    }
-    else if(selected==="inactiveBtn") {
-        btnStyle= "category-btn-container-no-select";
-        iconStyle="gray-plus";
-        Icon=FeatherIcon["FiPlus"];
-    }
-    else if (selected==="listBtn"){
+    if (type==="bubble"){
+        if (select===true || selected===true){
+            btnStyle= "category-btn-container-select";
+            iconStyle="white-plus";
+            Icon=FeatherIcon["FiCheck"];
+        }
+        else {
+            btnStyle= "category-btn-container-no-select";
+            iconStyle="gray-plus";
+            Icon=FeatherIcon["FiPlus"];
+        }
+    } else {
         btnStyle= "list-btn-container";
         iconStyle="white-plus";
         Icon=FeatherIcon["FiX"];
     }
      return(
-            <div className={"category-btn-container" + " " + btnStyle}>
+            <div className={"category-btn-container" + " " + btnStyle + " " + (removed&&"removed-category")} onClick={()=>{
+                !selected?setSelect(!select):(setRemoved(true));
+            }}>
                 <p>{name}</p>
                 <Icon className ={"category-icon" + " " + iconStyle} size={22}/>
             </div>
@@ -31,5 +37,6 @@ export default function CategoryBtn({name,selected,menuIcon}) {
 
 CategoryBtn.defaultProps = {
     name:"Default Category",
-    selected:"inactiveBtn"
+    type: "bubble",
+    selected: false
 };
