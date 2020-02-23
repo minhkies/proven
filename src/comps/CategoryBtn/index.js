bb4ed8d41e4bec645d9e8eeb4768dfa36563df77
abhimannyucc <abhimannyucc@gmail.com>
2020-02-22 17:38:20 -0800

    fix

--- src/comps/CategoryBtn/index.js
+++ src/comps/CategoryBtn/index.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-import React,{useState} from "react";
-import * as FeatherIcon from "react-icons/fi";
-import "../font.scss"
-export default function CategoryBtn({name,selected,menuIcon,onClick}) {
-    const [state,setState] = useState(false);
-        if (!state){
-            selected="inactiveBtn"
-            setState(true);
-        }
-        else {
-            selected="activeBtn"
-            setState(false);
-        }
-    
-=======
+
 import React, {useState} from "react";
 import * as FeatherIcon from "react-icons/fi";
 import "../font.scss"
@@ -21,7 +6,6 @@ export default function CategoryBtn({name, type, selected, menuIcon}) {
     let [removed, setRemoved] = useState(false);
     let [select, setSelect] = useState(false);
 
->>>>>>> b23894e7bae829192104c5b7ab6a28004a7f7c01
     let Icon = FeatherIcon[menuIcon];
     let btnStyle = null;
     let iconStyle = null;
@@ -44,13 +28,9 @@ export default function CategoryBtn({name, type, selected, menuIcon}) {
     }
   
      return(
-<<<<<<< HEAD
-            <div className={"category-btn-container" + " " + btnStyle} onClick={()=>setState(!state)}>
-=======
             <div className={"category-btn-container" + " " + btnStyle + " " + (removed&&"removed-category")} onClick={()=>{
                 !selected?setSelect(!select):(setRemoved(true));
             }}>
->>>>>>> b23894e7bae829192104c5b7ab6a28004a7f7c01
                 <p>{name}</p>
                 <Icon className ={"category-icon" + " " + iconStyle} size={22}/>
             </div>
@@ -59,11 +39,6 @@ export default function CategoryBtn({name, type, selected, menuIcon}) {
 
 CategoryBtn.defaultProps = {
     name:"Default Category",
-<<<<<<< HEAD
-    selected:"inactiveBtn",
-    onClick: ()=>{}
-=======
     type: "bubble",
     selected: false
->>>>>>> b23894e7bae829192104c5b7ab6a28004a7f7c01
 };
